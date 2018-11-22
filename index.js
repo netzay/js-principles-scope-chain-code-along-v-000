37d02a716827f8de853ae1325ab8c53221a77950
Ally Netzel <netzelally@gmail.com>
2018-11-22 17:36:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function overWriteBestCustomer(){
   bestCustomer = 'maybe bob';
 }
 
-var leastFavoriteCustomer= 'maybe bob';
+const leastFavoriteCustomer= 'maybe bob';
 function changeLeastFavoriteCustomer(){
   leastFavoriteCustomer = 'changed';
 }
